f664f40a2fab19d1777248de143b92994b8910ea
Chii Ayano <ayano@l.udonge.net>
2023-03-21 23:23:56 +0900

    修正

---
+++ ExifReader/Models/Enums/ExposureProgram.cs
@@ -0,0 +1,44 @@
+﻿namespace ExifReader.Models.Enums;
+
+/// <summary>
+/// 露出プログラム定義
+/// </summary>
+internal enum ExposureProgram
+{
+    /// <summary>
+    /// 未定義
+    /// </summary>
+    Undefined = 0,
+    /// <summary>
+    /// マニュアルモード
+    /// </summary>
+    Manual = 1,
+    /// <summary>
+    /// プログラムモード
+    /// </summary>
+    NormalProgram = 2,
+    /// <summary>
+    /// 絞り優先モード
+    /// </summary>
+    Aperture = 3,
+    /// <summary>
+    /// シャッター優先モード
+    /// </summary>
+    Shutter = 4,
+    /// <summary>
+    /// クリエイティブプログラムモード
+    /// </summary>
+    CreativeProgram = 5,
+    /// <summary>
+    /// アクションプログラムモード
+    /// </summary>
+    ActionProgram = 6,
+    /// <summary>
+    /// ポートレートモード
+    /// </summary>
+    Portrait = 7,
+    /// <summary>
+    /// ランドスケープモード
+    /// </summary>
+    Landscape = 8,
+}
--- ExifReader/Models/Tags/ExposureProgramTag.cs
+++ ExifReader/Models/Tags/ExposureProgramTag.cs
@@ -1,4 +1,6 @@
-﻿namespace ExifReader.Models.Tags;
+﻿using ExifReader.Models.Enums;
+
+namespace ExifReader.Models.Tags;
 
 /// <summary>
 /// 露出プログラムを表すタグ
@@ -28,47 +30,4 @@ internal record ExposureProgramTag : TagBase
         this.TagId = tagId;
         this.Program = (ExposureProgram)data;
     }
-}
-
-/// <summary>
-/// 露出プログラム定義
-/// </summary>
-public enum ExposureProgram
-{
-    /// <summary>
-    /// 未定義
-    /// </summary>
-    Undefined = 0,
-    /// <summary>
-    /// マニュアルモード
-    /// </summary>
-    Manual = 1,
-    /// <summary>
-    /// プログラムモード
-    /// </summary>
-    NormalProgram = 2,
-    /// <summary>
-    /// 絞り優先モード
-    /// </summary>
-    Aperture = 3,
-    /// <summary>
-    /// シャッター優先モード
-    /// </summary>
-    Shutter = 4,
-    /// <summary>
-    /// クリエイティブプログラムモード
-    /// </summary>
-    CreativeProgram = 5,
-    /// <summary>
-    /// アクションプログラムモード
-    /// </summary>
-    ActionProgram = 6,
-    /// <summary>
-    /// ポートレートモード
-    /// </summary>
-    Portrait = 7,
-    /// <summary>
-    /// ランドスケープモード
-    /// </summary>
-    Landscape = 8,
 }
